03d15ad3146edf24c642448527462c862f8ac5d5
Samuel de Sousa Almeida <samuel.sb04g@gmail.com>
2020-04-15 18:21:33 -0300

    2° parte do front-end
    criação da tela de Logon e de Cadastro

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,19 +1,13 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+import './global.css'
+import Routes from './routes';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment(){
-    setCounter(counter + 1);
-  }
 
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Routes />
   );
+
 }
 
 export default App;
---
+++ aulas/frontend/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Logon from './pages/Logon';
+import Register from './pages/Register'
+
+//as rotas também são componentes
+export default function Routes(){
+    return (
+        //BrowserRouter - basicamente está envolvendo todas as demais rotas
+        <BrowserRouter>
+            {/* Switch - faz com que as rotas sejam executadas 1 de cada vez */}
+            <Switch>
+                {/* Router - é a primeira rota a ser executada, por isso no path só tem um barra 
+                           - a proprieda 'exact' garante que o caminho tem que ser exatamente ao path,
+                             pois o react-router-dom não verifica se o caminho é igual ao path, mas sim se o início é igual.*/}
+                <Route path="/" exact component={Logon} />
+                <Route path="/Register" component={Register} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
