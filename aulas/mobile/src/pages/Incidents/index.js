8e2b299a5bfba81e2946454799a96e6b54d6f643
Samuel de Sousa Almeida <samuel.sb04g@gmail.com>
2020-04-28 07:51:16 -0300

    app finalizado
    conclusão das configurações do mobile

--- aulas/mobile/src/pages/Incidents/index.js
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -1,24 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 import { Feather } from '@expo/vector-icons';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 export default function Incidents() {
+    //sempre bom iniciar o State com o mesmo tipo da informação que vamos preencher ele depois. Neste caso um array vazio.
+    const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1); //controla paginação
+    const [loading, setLoading] = useState(false); //controla a busca de dados para evitar que sejam consultados repetidamente
+
     const navigation = useNavigation();
 
-    function navigationToDetail(){
-        navigation.navigate('Detail'); //'Detail' -> é o mesmo nome da rota definido em routes.js 
+    function navigationToDetail(incident){
+        navigation.navigate('Detail', { incident }); //'Detail' -> é o mesmo nome da rota definido em routes.js 
     }
 
-    async function loadIncidests(){
-        
+    async function loadIncidents(){
+        //validações abaixo evite busca descessária dos casos
+        if(loading){ return; }
+        if(total > 0 && incidents.length == total) { return ; }
+
+        setLoading(true);
+
+        const response = await api.get('incidents', {
+            params: { page },
+        });
+
+        //abaixo o três pontos ... permite anexar vetores, neste caso os dados das pages 1, 2, 3 etc.. 
+        //ou seja, está anexando o array inicial com o da próxima consulta
+        setIncidents([...incidents, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+
+        setPage(page + 1);
+        setLoading(false);
     }
     
     useEffect(() => {
-
+        loadIncidents();
     }, []);
 
     return (
@@ -26,7 +50,7 @@ export default function Incidents() {
             <View style={styles.header}>
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
                 </Text>
             </View>
 
@@ -34,24 +58,29 @@ export default function Incidents() {
             <Text style={styles.description}>Escolha um caso um dos casos abaixo e salve o dia.</Text>
 
             <FlatList 
-                data={[1, 2, 3]}
+                data={incidents} //array com os dados
                 style={styles.incidentList}
-                keyExtractor={incident => String(incident)} /* passar o ID o incident */
+                keyExtractor={incident => String(incident.id)} /* passar o ID o incident */
                 showsVerticalScrollIndicator={false} /* exibe ou não a barra de scroll */
-                renderItem={()=> (
+                onEndReached={loadIncidents} /* dispara quando chega ao final da lista */
+                onEndReachedThreshold={0.2} /* quantos porcentos do final da lista o user deve estar para carregar novos casos na lista, neste caso 20%*/
+                renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
-                        <Text style={styles.incidentValue}>APAD</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
                         <Text style={styles.incidentProperty}>CASO:</Text>
-                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>VALOR:</Text>
-                        <Text style={styles.incidentValue}>RS 120,00</Text>
+                        <Text style={styles.incidentValue}>
+                            {Intl.NumberFormat('pt-BR', { 
+                                style: 'currency', currency:'BRL'}).format(incident.value)
+                        }</Text>
 
                         <TouchableOpacity 
                             style={styles.detailsButton} 
-                            onPress={navigationToDetail} /* função de navegação para tela de Detail */ 
+                            onPress={ () => navigationToDetail(incident)} /* função de navegação para tela de Detail */ 
                         >
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                             <Feather name="arrow-right" size={16} color="#E02041"></Feather>
